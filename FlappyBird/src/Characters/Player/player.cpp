761559310caee9733538bf8d496433c23fb6b760
Ivan Castellano <justdarsan@gmail.com>
2018-10-24 21:25:06 -0300

    Added top screen limit

--- FlappyBird/src/Characters/Player/player.cpp
+++ FlappyBird/src/Characters/Player/player.cpp
@@ -14,12 +14,14 @@ namespace flappybird {
 		Player player;
 
 		bool isMoving;
-
 		float delayTime = 0.1f;
-		int GRAVITY = 4;
-		float PLAYER_SPEED = 100;
-		float PLAYER_ROTATION_SPEED = 300.0f;
 		int currentFrame = 0;
+
+		
+		int GRAVITY = 4;
+		int PLAYER_ON_CLICK_ROTATION = -45;
+		float PLAYER_SPEED = -250;
+		
 
 		static float timer; //Animations coming soon
 		
@@ -59,31 +61,29 @@ namespace flappybird {
 
 		void update() {
 			timer += GetFrameTime();
-
-			mousePosition = GetMousePosition();
-
-			//Distancia entre player y mouse
-			U = {  mousePosition.x - player.position.x  , GetScreenHeight() - player.position.y };
-
-			//player.rotation = (atan2(U.y,U.x)*RAD2DEG);
 	
 			// Player logic: acceleration
 			if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
 				
 				if (player.position.y >= player.texture.height) {
-					player.rotation = -45;
-					player.speed.y = -250 * GetFrameTime();
+					player.rotation = PLAYER_ON_CLICK_ROTATION;
+					player.speed.y = PLAYER_SPEED * GetFrameTime();
+
+					if (player.position.y <= player.texture.height) {
+						player.position.y = player.texture.height;
+					}
 
 					currentFrame = 1;
 					player.sourceRec.x = (float)currentFrame*(float)player.texture.width / 3;
 					timer = 0;
 				}
+				
 
 			}
 			else {
 				if (timer > delayTime) {
 					if (player.rotation < 90) {
-						player.rotation += 1 ;
+						player.rotation += 1;
 					}
 					
 					currentFrame = 0;
